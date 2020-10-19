36dc23afd82694e0948badd2cd8b2ed3f8d294e3
Brian Staruk <brian@staruk.net>
2020-10-19 18:20:16 -0400

    redirect logged-in users

--- src/app/containers/LoginPage/index.js
+++ src/app/containers/LoginPage/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 import { H1 } from 'app/components/Type';
 
+import { selectUserAuthenticated } from 'app/data/user/selectors';
+
 export function LoginPage() {
+  const userAuthenticated = useSelector(selectUserAuthenticated);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (userAuthenticated) {
+      history.replace('/');
+    }
+  }, [history, userAuthenticated]);
+
   return (
     <>
       <Helmet>

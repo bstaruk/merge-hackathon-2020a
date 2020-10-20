9b117b8ae0c383c6e94917f541772cc4c3ebbf8d
Brian Staruk <brian@staruk.net>
2020-10-20 08:28:46 -0400

    responsive css tweaks for login page

--- src/app/containers/LoginPage/wrappers.js
+++ src/app/containers/LoginPage/wrappers.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/macro';
+import { mq } from 'utils/style';
 
 export const LoadingWrapper = styled.div`
   position: absolute;
@@ -24,6 +25,7 @@ export const LoadingWrapper = styled.div`
 export const PageWrapper = styled.main`
   width: 100%;
   height: 100vh;
+  padding: 1rem;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -32,10 +34,18 @@ export const PageWrapper = styled.main`
 
 export const HeaderWrapper = styled.header`
   text-align: center;
-  margin-bottom: 3rem;
+  margin-bottom: 1.25rem;
 
   & > * {
     margin: 0;
+  }
+
+  @media ${mq.mobileLg} {
+    margin-bottom: 2rem;
+  }
+
+  @media ${mq.tablet} {
+    margin-bottom: 3rem;
   }
 `;
 
@@ -46,16 +56,28 @@ export const FormWrapper = styled.form`
   align-items: center;
   max-width: 30rem;
   width: 100%;
-  padding: 3rem;
+  padding: 2.25rem 1.5rem;
   margin: 0 auto;
   background-color: white;
   border-radius: 0.25rem;
   box-shadow: 5px 5px 15px rgba(11, 11, 11, 0.1);
+
+  @media ${mq.mobileLg} {
+    padding: 3rem 2rem;
+  }
+
+  @media ${mq.tablet} {
+    padding: 3rem;
+  }
 `;
 
 export const FormFieldWrapper = styled.div`
   width: 100%;
-  margin-bottom: 1.5rem;
+  margin-bottom: 1rem;
+
+  @media ${mq.tablet} {
+    margin-bottom: 1.5rem;
+  }
 `;
 
 export const FormSubmitWrapper = styled.div`

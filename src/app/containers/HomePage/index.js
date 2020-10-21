3cc5f131ef30a3c168cbcab186d6181fd05fb5d5
Brian Staruk <brian@staruk.net>
2020-10-21 10:03:59 -0400

    icons buildout

--- src/app/containers/HomePage/index.js
+++ src/app/containers/HomePage/index.js
@@ -35,7 +35,7 @@ export function HomePage() {
   }, [history, userAuthenticated]);
 
   useEffect(() => {
-    const timer = setTimeout(() => setReminderModalOpen(true), 3000);
+    const timer = setTimeout(() => setReminderModalOpen(true), 2000);
     return () => clearTimeout(timer);
   }, []);
 
